948dbace44c3a7d214b2b8abe699882d2846683d
Daniel Arneman <daniel@plotwatt.com>
2019-03-19 15:59:13 -0400

    setup file to make it installable

---
+++ setup.py
@@ -0,0 +1,22 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="plotz",
+    version="0.0.1",
+    author="Daniel Arneman",
+    author_email=None,
+    description="Graphing shortcuts with Pandas and Plotly",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dkarneman/plotz",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
